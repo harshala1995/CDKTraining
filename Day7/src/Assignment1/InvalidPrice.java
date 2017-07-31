c2ad3060a724419a68b2c546b9d2d99ab89354fb
arunkhot <arunkhot86@gmail.com>
2017-07-31 18:01:45 +0530

    Update InvalidPrice.java

--- Day7/src/Assignment1/InvalidPrice.java
+++ Day7/src/Assignment1/InvalidPrice.java
@@ -3,6 +3,7 @@ package Assignment1;
 /**
  * Created by chavanh on 7/26/2017.
  */
+// InvalidPriceException would be proper name for this class. 
 public class InvalidPrice extends Exception {
     public InvalidPrice() {
     }
