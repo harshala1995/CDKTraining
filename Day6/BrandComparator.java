b5eb5bfbbafffba53d79ec87d4bb9f0e92e4ee5a
arunkhot <arunkhot86@gmail.com>
2017-07-31 17:56:49 +0530

    Update BrandComparator.java

--- Day6/BrandComparator.java
+++ Day6/BrandComparator.java
@@ -7,6 +7,7 @@ public class BrandComparator implements Comparator<Car> {
     @Override
     public int compare(Car c1, Car c2) {
         int i = c1.getBrand().compareTo(c2.getBrand());
+        // you can directly return output of result of above statement instead of creating new variable.
         return i;
     }
 }
